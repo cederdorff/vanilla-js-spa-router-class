04a5a78bb15e8bd00663644bb4951aca8f1c9d07
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-11-29 19:58:09 +0100

    init

---
+++ router.js
@@ -0,0 +1,85 @@
+class Router {
+    // private attributes
+    #basePath = location.pathname.replace("index.html", ""); // remove index.html from path
+    #pages = document.querySelectorAll(".page"); // reference to alle page sections in the DOM
+    #navLinks = document.querySelectorAll("nav a"); // reference to all nav links
+
+    constructor() {
+        this.routes = {
+            /**
+             * All routes of the SPA
+             * "path": "id of page in DOM"
+             */
+            "#/": "home",
+            "#/about": "about",
+            "#/clients": "clients",
+            "#/contact": "contact"
+        };
+        this.initRouter();
+    }
+    /**
+     * Changing display to none for all pages
+     */
+    hideAllPages() {
+        for (const page of this.#pages) {
+            page.style.display = "none";
+        }
+    }
+
+    /**
+     * Navigating SPA to specific page by given path
+     */
+    navigateTo(path) {
+        if (!this.routes[location.hash]) {
+            path = "#/"; // default path
+        }
+        window.history.pushState({}, path, this.#basePath + path);
+        this.showPage(path);
+    }
+
+    /**
+     * Displaying page by given path
+     */
+    showPage(path) {
+        this.hideAllPages(); // hide all pages
+        document.querySelector(`#${this.routes[path]}`).style.display = "block"; // show page by given path
+        this.setActiveTab(path);
+    }
+
+    /**
+     * sets active menu item by given path
+     */
+    setActiveTab(path) {
+        for (const link of this.#navLinks) {
+            if (path === link.getAttribute("href")) {
+                link.classList.add("active");
+            } else {
+                link.classList.remove("active");
+            }
+        }
+    }
+
+    /**
+     * Attaching event to nav links and preventing default anchor link event
+     */
+    attachNavLinkEvents() {
+        for (const link of this.#navLinks) {
+            link.addEventListener("click", event => {
+                event.preventDefault();
+                const path = link.getAttribute("href");
+                this.navigateTo(path);
+            });
+        }
+    }
+
+    /**
+     * Initialising the router, calling attachNavLinkEvents(), popstate event and navigateTo()
+     */
+    initRouter() {
+        this.attachNavLinkEvents();
+        window.addEventListener("popstate", () => this.navigateTo(location.hash)); // change page when using back and forth in browser
+        this.navigateTo("#/");
+    }
+}
+
+const router = new Router();
